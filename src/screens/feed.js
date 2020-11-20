dd756a67b4b8cb7d8feeaeedbe96d2cf74fd8a71
asteph <alissa.stephens1@gmail.com>
2020-11-19 20:03:41 -0600

    First commit Create Expo project from scratch since had compatability issues with expo app version and fork from https://github.com/javascripteverywhere/mobile Add mobile Navigation tabs and stacks

---
+++ src/screens/feed.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text, View, Button } from "react-native";
+
+const Feed = (props) => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Text>Note Feed</Text>
+    <Button
+      title="Keep reading"
+      onPress={() => props.navigation.navigate('Note')}
+    />
+  </View>
+  )
+}
+
+export default Feed;
---
+++ src/screens/note.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from "react-native";
+
+const NoteScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Text>This is a note!</Text>
+  </View>
+  )
+}
+
+export default NoteScreen;
