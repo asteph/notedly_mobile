a053609f10b2b3f632442833fd81b32ca5e21b20
asteph <alissa.stephens1@gmail.com>
2020-11-21 12:04:43 -0600

    Add native loading indicator

--- src/screens/feed.js
+++ src/screens/feed.js
@@ -3,6 +3,7 @@ import { Text } from 'react-native';
 import { useQuery } from '@apollo/client';
 
 import NoteFeed from '../components/NoteFeed';
+import Loading from '../components/Loading';
 import { GET_NOTES } from '../gql/query';
 
 const Feed = (props) => {
@@ -10,7 +11,7 @@ const Feed = (props) => {
   const { loading, error, data } = useQuery(GET_NOTES);
 
   // if the data is loading, display a loading message
-  if (loading) return <Text>Loading...</Text>;
+  if (loading) return <Loading />;
   // if there is an error fetching the data, display an error message
   if (error) return <Text>Error loading notes</Text>;
 
--- src/screens/note.js
+++ src/screens/note.js
@@ -3,13 +3,14 @@ import { Text, View } from 'react-native';
 import { useQuery } from '@apollo/client';
 
 import Note from '../components/Note';
+import Loading from '../components/Loading';
 import { GET_NOTE } from '../gql/query';
 
 const NoteScreen = (props) => {
   const id = props.navigation.getParam('id');
   const { loading, error, data } = useQuery(GET_NOTE, { variables: { id } });
 
-  if (loading) return <Text>Loading...</Text>;
+  if (loading) return <Loading />;
   if (error) return <Text>Error, note not found</Text>;
 
   return <Note note={data.note} />;
